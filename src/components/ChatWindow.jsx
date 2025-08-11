c15161e1d619057ba99cf077440bc758e56ca815
HemavathyLakisha <hemaluki@gmail.com>
2025-08-11 23:41:41 +0530

    updated header

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -74,13 +74,17 @@ export default function ChatWindow() {
         </div>
 
         {/* Main Content */}
+       
         <div className="col p-4">
+             {/* Page Header */}
+         <h1 className="fw-bold mb-3">Bot AI</h1>
+
           {/* Chat Window */}
           <div
             className="chat-container mb-3 d-flex flex-column"
             style={{ height: "400px", overflowY: "auto" }}
           >
-             <h5 className="fw-bold">Bot AI</h5>
+             {/* <h5 className="fw-bold">Bot AI</h5> */}
             {/* Quick Suggestions */}
             {messages.length === 0 && (
               <div className="text-center mb-3 mt-5">
