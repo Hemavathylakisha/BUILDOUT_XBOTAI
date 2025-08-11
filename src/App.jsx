cf50331fbfce705ad0b3edaa63f9cd5680d0a29a
HemavathyLakisha <hemaluki@gmail.com>
2025-08-11 23:25:09 +0530

    Botai app created

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ChatWindow from "./components/ChatWindow";
+import PastConversations from "./components/PastConversations";
+import FeedbackList from "./components/FeedbackList";
+
+export default function App() {
+  return (
+    <Router>
+      {/* <nav className="p-4 bg-gray-200 flex gap-4">
+        <Link to="/">Chat with Bot</Link>
+        <Link to="/history">Past Conversations</Link>
+        <Link to="/feedback">All Feedback</Link>
+      </nav> */}
+      <Routes>
+        <Route path="/" element={<ChatWindow />} />
+        <Route path="/history" element={<PastConversations />} />
+        <Route path="/feedback" element={<FeedbackList />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
